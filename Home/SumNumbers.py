d2318069fc9310c11f7cb3a91f77ca81850af187
MartynovDD <dmitrii_martynov@epam.com>
2020-06-08 14:20:47 +0400

    Initial commit

---
+++ Home/SumNumbers.py
@@ -0,0 +1,7 @@
+"""
+In a given text you need to sum the numbers. 
+Only separated numbers should be counted. 
+If a number is part of a word it shouldn't be counted.
+The text consists from numbers, spaces and english letters
+https://py.checkio.org/ru/mission/sum-numbers/
+"""

f3ddc7be68f7702cf6ba65f2d3924e8d6e73754a
MartynovDD <dmitrii_martynov@epam.com>
2020-06-09 13:09:24 +0400

    Started reworking task

---
+++ home/sum_numbers.py
@@ -0,0 +1,25 @@
+"""
+In a given text you need to sum the numbers. 
+Only separated numbers should be counted. 
+If a number is part of a word it shouldn't be counted.
+The text consists from numbers, spaces and english letters
+https://py.checkio.org/ru/mission/sum-numbers/
+"""
+
+import re
+
+
+def sum_numbers(text):
+    pass
+
+
+def sum_numbers_regex(text):
+    pass
+
+
+def unified_sum_func(text):
+    pass
+
+
+if __name__ == "__main__":
+    pass

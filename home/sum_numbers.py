ff2e9e4700a281ff3e8ba646849ee2590c9568b8
MartynovDD <dmitrii_martynov@epam.com>
2020-06-09 16:20:39 +0400

    Added docstrings and tests

--- home/sum_numbers.py
+++ home/sum_numbers.py
@@ -10,16 +10,33 @@ import re
 
 
 def sum_numbers(text):
+    """
+    Sums all separated digits in given string.
+    Numbers that are part of the word are not counted.
+    """
     pass
 
 
 def sum_numbers_regex(text):
+    """
+    Does the same as 'sum_numbers()' funcs but using regex
+    """
     pass
 
 
 def unified_sum_func(text):
+    """
+    Compares results of 'sum_numbers()' and 'sum_numbers_regex()' funcs
+    and returns sum of numbers in text if both results are equal
+    """
+
     pass
 
 
 if __name__ == "__main__":
-    pass
+    assert unified_sum_func("") == 0  # Case 1
+    assert unified_sum_func("test") == 0  # Case 2
+    assert unified_sum_func("1") == 1  # Case 3
+    assert unified_sum_func("test123 5") == 5  # Case 4
+    assert unified_sum_func("421 1111") == 1532  # Case 5
+    assert unified_sum_func("123foo 1 bar100 3 4spam6 6 ") == 10  # Case 6

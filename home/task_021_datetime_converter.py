9226182dc7b8847d2ea4a3bd3df3e15b28b7c6e7
MartynovDD <dmitrii_martynov@epam.com>
2020-07-08 18:21:25 +0400

    Done task 021

--- home/task_021_datetime_converter.py
+++ home/task_021_datetime_converter.py
@@ -4,6 +4,7 @@ Humans prefer to see something like this: 21 May 2018 year, 16 hours 30 minutes
 Your task is simple - convert the input date and time from computer format into a "human" format.
 https://py.checkio.org/en/mission/date-and-time-converter/
 """
+from datetime import datetime
 
 
 def date_time(time: str) -> str:
@@ -12,4 +13,7 @@ def date_time(time: str) -> str:
     :param time: String containing date and time to process
     :return: Datetime string in human readable format (ex: 1 January 2000 year 0 hours 0 minutes)
     """
-    return time
+    dt = datetime.strptime(time, "%d.%m.%Y %H:%M")
+    hour = "hour" if dt.hour == 1 else "hours"
+    minute = "minute" if dt.minute == 1 else "minutes"
+    return dt.strftime("{} %B %Y year {} {} {} {}").format(dt.day, dt.hour, hour, dt.minute, minute)

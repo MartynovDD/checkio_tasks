ce8ae636ada02a917ca5ff8ab7e5bba5b63566e9
MartynovDD <dmitrii_martynov@epam.com>
2020-07-23 15:56:50 +0400

    Done task 005 and tests (electronic station)

--- electronic_station/task_005_ascending_list.py
+++ electronic_station/task_005_ascending_list.py
@@ -10,9 +10,17 @@ from typing import Iterable
 from utils.check_type import check_type
 
 
-def is_ascending(items: Iterable) -> bool:
+def is_ascending(items: Iterable[int]) -> bool:
     """
-
+    Checks if all items in given iterable are in ascending order
     :param items:
     :return:
     """
+    assert isinstance(items, Iterable)
+
+    ascending = True
+    for index in range(len(items) - 1):
+        if items[index] >= items[index + 1]:
+            ascending = False
+
+    return ascending
--- tests/electronic_station_tests/test_005_ascending_list.py
+++ tests/electronic_station_tests/test_005_ascending_list.py
@@ -1,3 +1,32 @@
 import pytest
 from electronic_station.task_005_ascending_list import is_ascending
-from hypothesis import given, Verbosity, settings, strategies as st
+
+
+def test_ascending():
+    assert is_ascending([-5, 10, 99, 123456]), "Ascending items"
+
+
+def test_descending():
+    assert not is_ascending((9, 8, 7, 6, 5)), "Descending items"
+
+
+def test_mixed():
+    assert not is_ascending([4, 5, 6, 7, 3, 7, 9]), "Mixed items"
+
+
+def test_equal():
+    assert not is_ascending([0, 0, 0, 0]), "Equal items"
+
+
+def test_empty_list():
+    assert is_ascending([]), "Empty list"
+
+
+def test_no_args():
+    with pytest.raises(TypeError):
+        is_ascending()
+
+
+def test_incorrect_args():
+    with pytest.raises(AssertionError):
+        is_ascending(3)

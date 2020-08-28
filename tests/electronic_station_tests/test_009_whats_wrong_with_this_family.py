e90caea11eb00d320d49fbb64d96ec8bc85e1b93
MartynovDD <dmitrii_martynov@epam.com>
2020-08-28 11:42:55 +0400

    Updated tests for task 009

--- tests/electronic_station_tests/test_009_whats_wrong_with_this_family.py
+++ tests/electronic_station_tests/test_009_whats_wrong_with_this_family.py
@@ -1,84 +1,65 @@
 import pytest
 from electronic_station.task_009_whats_wrong_with_this_family import is_family
-from hypothesis import given, settings, Verbosity, example, strategies as st
+from hypothesis import given, settings, Verbosity, assume, example, strategies as st
+
+name_strategy = st.text(alphabet=st.characters(whitelist_categories=("Ll", "Lu")))
 
 
-@given(father_and_son=st.lists(st.lists(st.text(st.characters(whitelist_categories=("Ll", "Lu"))),
-                                        st.text(st.characters(whitelist_categories=("Ll", "Lu"))),
-                                        unique=True, min_size=2),
-                               min_size=1))
-@example(father_and_son=[["Alex", "Jake"]])
+def test_father_and_son():
+    assert is_family([["Alex", "Jake"]]), "Father and son"
+
+
+@given(st.data())
 @settings(verbosity=Verbosity.verbose)
-def test_father_and_son(father_and_son):
-    assert is_family(father_and_son), "Father and son"
+def test_two_sons(data):
+    father = data.draw(name_strategy)
+    first_son = data.draw(name_strategy)
+    second_son = data.draw(name_strategy)
+
+    assume(father != first_son)
+    assume(father != second_son)
+    assume(first_son != second_son)
+
+    tree = [[father, first_son], [father, second_son]]
+
+    assert is_family(tree), "Two sons of one father"
 
 
-@given(father=st.text(alphabet=st.characters(whitelist_categories=("Ll", "Lu"))),
-       son=st.text(alphabet=st.characters(whitelist_categories=("Ll", "Lu"))))
+def test_grandfather():
+    tree = [["Alex", "Jake"], ["Alex", "William"], ["William", "Ron"]]
+    assert is_family(tree), "Grandfather and grandson"
+
+
+@given(name=name_strategy)
 @settings(verbosity=Verbosity.verbose)
-def test_two_sons(father, son):
-    first_son = st.shared(son)
-    second_son = st.shared(son)
-    people = [[father, first_son], [father, second_son]]
+def test_equal_names(name):
+    father = son = name
+    tree = [[father, son]]
 
-    assert is_family(people), "Two sons of one father"
+    assert not is_family(tree), "Equal names"
 
 
-@given(father=st.text(alphabet=st.characters(whitelist_categories=("Ll", "Lu"))),
-       son=st.text(alphabet=st.characters(whitelist_categories=("Ll", "Lu"))))
+def test_father_to_father():
+    tree = [["Alex", "Jake"], ["Alex", "Ron"], ["Ron", "Alex"]]
+    assert not is_family(tree), "Second brother is father of father"
+
+
+def test_father_to_brother():
+    tree = [["Alex", "Jake"], ["Alex", "Ron"], ["Ron", "Jake"]]
+
+    assert not is_family(tree), "Second brother is father of first brother"
+
+
+def test_strangers_in_family():
+    tree = [["Alex", "Jake"], ["Jake", "Ron"], ["Mario", "Luigi"]]
+    assert not is_family(tree)
+
+
+@given(tree=st.lists(st.lists(st.nothing())))
 @settings(verbosity=Verbosity.verbose)
-def test_grandfather(father, son):
-    first_brother = st.shared(son)
-    second_brother = st.shared(son)
-    son_of_second_brother = st.shared(second_brother)
-    people = [[father, first_brother], [father, second_brother], [second_brother, son_of_second_brother]]
-
-    assert is_family(people), "Grandfather and grandson"
-
-
-@given(father=st.text(alphabet=st.characters(whitelist_categories=("Ll", "Lu"))),
-       son=st.text(alphabet=st.characters(whitelist_categories=("Ll", "Lu"))))
-@settings(verbosity=Verbosity.verbose)
-def test_father_to_father(father, son):
-    first_brother = st.shared(son)
-    second_brother = st.shared(son)
-    people = [[father, first_brother], [father, second_brother], [second_brother, father]]
-
-    assert not is_family(people), "Second brother is father of father"
-
-
-@given(father=st.text(alphabet=st.characters(whitelist_categories=("Ll", "Lu"))),
-       son=st.text(alphabet=st.characters(whitelist_categories=("Ll", "Lu"))))
-@settings(verbosity=Verbosity.verbose)
-def test_father_to_brother(father, son):
-    first_brother = st.shared(son)
-    second_brother = st.shared(son)
-    people = [[father, first_brother], [father, second_brother], [second_brother, first_brother]]
-
-    assert not is_family(people), "Second brother is father of first brother"
-
-
-@given(father=st.text(alphabet=st.characters(whitelist_categories=("Ll", "Lu"))),
-       son=st.text(alphabet=st.characters(whitelist_categories=("Ll", "Lu"))))
-@settings(verbosity=Verbosity.verbose)
-def test_strangers_in_family(father, son):
-    first_brother = st.shared(son)
-    second_brother = st.shared(son)
-    son_of_second_brother = st.shared(second_brother)
-    unknown_father = st.shared(father)
-    unknown_son = st.shared(son)
-    people = [[father, first_brother],
-              [father, second_brother],
-              [second_brother, son_of_second_brother],
-              [unknown_father, unknown_son]]
-
-    assert not is_family(people)
-
-
-@given(people=st.lists(st.lists(st.nothing())))
-@settings(verbosity=Verbosity.verbose)
-def test_empty_lists(people):
-    assert not is_family(people), "Empty lists"
+def test_empty_lists(tree):
+    with pytest.raises(ValueError):
+        is_family(tree)
 
 
 def test_no_arguments():
@@ -86,8 +67,8 @@ def test_no_arguments():
         is_family()
 
 
-@given(people=st.iterables(st.iterables(st.integers(), st.floats(), st.none(), st.booleans())))
+@given(tree=st.iterables(st.iterables(st.one_of(st.integers(), st.floats(), st.none(), st.booleans()))))
 @settings(verbosity=Verbosity.verbose)
-def test_incorrect_args(people):
+def test_incorrect_args(tree):
     with pytest.raises(TypeError):
-        is_family()
+        is_family(tree)
